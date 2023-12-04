87495c07e8036ee1ef96bae9fff1fc22226f1883
Milenna Cristina Nogueira Costa <milenna.costa@seedz.ag>
2023-12-04 13:38:37 -0300

    Create e2e tests with cypress

---
+++ cypress/e2e/authenticated.cy.js
@@ -0,0 +1,62 @@
+import { faker } from '@faker-js/faker/locale/en'
+
+describe('Scenarios where authentication is a pre-condition', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.sessionLogin()
+  })
+
+  it('CRUDs a note', () => {
+    const noteDescription = faker.lorem.words(4)
+
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
+
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
+
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
+
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
+  })
+
+  it.only('successfully submits the settings form', () => {
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+
+    cy.fillSettingsFormAndSubmit()
+
+    cy.wait('@getNotes')
+    cy.wait('@paymentRequest')
+      .its('state')
+      .should('be.equal', 'Complete')
+      
+  if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
+    cy.get('.navbar-toggle.collapsed')
+      .should('be.visible')
+      .click()
+}
+      
+  })
+})
+
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings')
+  cy.get('#storage').type('1')
+  cy.get('#name').type('Mary Doe')
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242')
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271')
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123')
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345')
+  cy.contains('button', 'Purchase').click()
+})
